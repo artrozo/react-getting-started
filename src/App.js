04f886a23d37a97ec714838d037a2de946574b70
Adarsh Raj <ar@trozo.io>
2021-02-04 23:26:27 +0000

    props: passing name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <h1>Hello World</h1>;
+function App({ name }) {
+  return <h1>Hello {name}</h1>;
 }
 
 export default App;
