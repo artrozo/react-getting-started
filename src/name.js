4eb41fbb1bc30f762d36f17409f17dee200dde75
Adarsh Raj <ar@trozo.io>
2021-02-05 09:21:33 +0000

    conditional: conditional rendering

--- src/name.js
+++ src/name.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 const Name = () => {
-  const [name, setName] = useState("");
+  const [name, setName] = useState("Raj");
   return (
     <div>
-      <h1>Hello {name}</h1>
+      {name === "Adarsh" ? (
+        <h1>Hello {name} Raj </h1>
+      ) : (
+        <h1>Hello Mr {name}</h1>
+      )}
+
       <input onChange={(e) => setName(e.target.value)} />
     </div>
   );
